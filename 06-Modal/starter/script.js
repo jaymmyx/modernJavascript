9dc53650e6983c510eea7ad1007025ae1c94d3b7
James Njoroge <njorogehjames20@gmail.com>
2025-10-28 09:43:01 +0300

    03-modal functionality(Escape key Event)

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,6 +4,7 @@ const showModal = document.querySelectorAll('.show-modal');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const closeModal = document.querySelector('.close-modal');
+let keylogger = '';
 
 for (let i = 0; i < showModal.length; i++) {
   showModal[i].addEventListener('click', () => {
@@ -21,3 +22,11 @@ const hideModal = elem => {
 
 hideModal(closeModal);
 hideModal(overlay);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+  keylogger += e.key;
+});
