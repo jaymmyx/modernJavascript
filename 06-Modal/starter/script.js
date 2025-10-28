a5f55a5bda15c027f0f34ef2aeb631887b0a359b
James Njoroge <njorogehjames20@gmail.com>
2025-10-28 09:16:34 +0300

    03-modal functionality

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const showModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+const hideModal = elem => {
+  elem.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });
+};
+
+hideModal(closeModal);
+hideModal(overlay);
